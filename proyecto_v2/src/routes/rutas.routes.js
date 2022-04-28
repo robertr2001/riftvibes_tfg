e2517b8926571aa0d2b96ff05a2a313778e89ec2
robertr2001 <robertrosiak2001@gmail.com>
2022-04-28 23:04:20 +0200

    Crear tabla campeonesLol añadido

--- proyecto_v2/src/routes/rutas.routes.js
+++ proyecto_v2/src/routes/rutas.routes.js
@@ -1,15 +1,24 @@
 const express =require("express");
 const router= express.Router();//devuelve un objeto donde puedes meter rutas
 const consultasRiot=require("../models/consultas_riot_api.js");
-const fetch=require("node-fetch");
+const champs=require("./champsIDs.json");
 const campeonLol=require("../models/modelo_bbdd");
 
-router.get("/:nombre",async(req,res)=>{//permite definir rutas en el servidor
-    let info=String(await consultasRiot.getSummonerByName(req.params.nombre, "euw1"));
-    console.log("El champ es"+info);
-    const nombreCampeon = await campeonLol.find({idCampeon: info}); //Por que esto no funciona si uso idCampeon???
-    console.log(nombreCampeon);
-    res.json(nombreCampeon);
+ router.get("/:nombre",async(req,res)=>{//permite definir rutas en el servidor
+     let info=String(await consultasRiot.getSummonerByName(req.params.nombre, "euw1"));
+     const nombreCampeon = await campeonLol.find({idCampeon: info}); //Por que esto no funciona si uso idCampeon???
+     console.log(nombreCampeon);
+     res.json(nombreCampeon);
+ });
+router.get("/bbdd",async(req,res)=>{//permite definir rutas en el servidor
+    campeonLol.remove()
+     champs.forEach(element => {
+          awaitcampeonLol.create([
+            element
+          ]);
+          console.log(element);
+    });
+
 });
 
 router.post("/",async (req,res)=>{
